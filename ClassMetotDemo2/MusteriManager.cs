00d9f167c4eaa58306d002c52fca92e3cc70e014
Nilüfer Çağla Yiğit <cagla.yigit@bilgiedu.net>
2021-01-23 14:44:54 +0300

    -

---
+++ ClassMetotDemo2/MusteriManager.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ClassMetotDemo2
+{
+    class MusteriManager
+    {
+        public void Ekle(Musteri musteri)
+        {
+            Console.WriteLine("Tebrikler. " + musteri.Adi + " " + musteri.Soyadi + " kişisi eklendi.");
+        }
+
+        public void Listele(Musteri[] musteriler)
+        {
+            foreach (Musteri musteri in musteriler)
+            {
+                Console.WriteLine(musteri.Id);
+                Console.WriteLine(musteri.Adi);
+                Console.WriteLine(musteri.Soyadi);
+
+            }
+        }
+
+        public void Sil(Musteri musteri)
+        {
+            Console.WriteLine(musteri.Adi + " " + musteri.Soyadi + " adlı kişi silindi.");
+        }
+    }
+}
--- ClassMetotDemo2/Program.cs
+++ ClassMetotDemo2/Program.cs
@@ -6,7 +6,27 @@ namespace ClassMetotDemo2
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Musteri musteri1 = new Musteri();
+            musteri1.Id = 483333;
+            musteri1.Adi = "Berna";
+            musteri1.Soyadi = "Dörtyüz";
+
+            Musteri musteri2 = new Musteri();
+            musteri2.Id = 190776;
+            musteri2.Adi = "Ferhat";
+            musteri2.Soyadi = "Teyar";
+
+            Musteri[] musteriler = new Musteri[] { musteri1, musteri2 };
+
+            MusteriManager musteriManager = new MusteriManager();
+            musteriManager.Ekle(musteri1);
+            musteriManager.Ekle(musteri2);
+            Console.WriteLine("------------------------");
+            musteriManager.Listele(musteriler);
+            Console.WriteLine("------------------------");
+            musteriManager.Sil(musteri1);
+            musteriManager.Sil(musteri2);
+
         }
     }
 }
